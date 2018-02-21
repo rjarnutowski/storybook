4edccf43adf7d4c58f3fc82169ff8a44f47e677c
Ryan Jarnutowski <rjarno@gmail.com>
2018-02-21 09:41:26 -0500

    adding passport google oauth functionality

--- app.js
+++ app.js
@@ -1,15 +1,25 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Passport Config
+require('./config/passport')(passport);
+
+//Route Files
+const auth = require('./routes/auth');
+
 
 const app = express();
 
-const port = process.env.port || 5000;
-
-app.get('/', (req, res) = > {
+app.get('/', (req, res) => {
     res.send('It Works');
 });
 
+// Use routes
+app.use('/auth', auth);
 
-app.listen(port, () = > {
+const port = process.env.port || 5000;
+
+app.listen(port, () => {
     console.log(`Server started on port ${port}`);
-}));
+});

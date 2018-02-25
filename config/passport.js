71015d97c1f23493b85230bc407052c18623c0b7
Ryan Jarnutowski <rjarno@gmail.com>
2018-02-25 18:51:36 -0500

    passport working

--- config/passport.js
+++ config/passport.js
@@ -1,50 +1,52 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
+// Load user model
 const User = mongoose.model('users');
 
 module.exports = function(passport){
-    passport.use(new GoogleStrategy({
-        clientID: keys.googleClientID,
-        clientSecret: keys.googleClientSecret,
-        callbackURL: 'auth/google/callback',
-        proxy: true
-    }, (accessToken, refreshToken, profile, done) => {
-        // console.log(accessToken);
-        // console.log(profile);
-        const image = profile.photos[0].value.substr(0, profile.photos[0].value.indexOf('?'));
+    passport.use(
+        new GoogleStrategy({
+            clientID: keys.googleClientID,
+            clientSecret:keys.googleClientSecret,
+            callbackURL:'/auth/google/callback',
+            proxy: true
+        }, (accessToken, refreshToken, profile, done) => {
+            // console.log(accessToken);
+            // console.log(profile);
 
-        const newUser = {
-            googleID: profile.name,
-            firstName: profile.name.givenName,
-            lastName: profile.name.familyName,
-            email: profile.emails[0].value,
-            image: image
-        }
+            const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
 
-        User.findOne({
-            googleID: profile.id
-        }).then(user => {
-            if (user) {
-                // Return User
-                done(null, user);
-            } else {
-                // Create User
-                new User(newUser)
-                    .save()
-                    .then(user => done(null, user))
+            const newUser = {
+                googleID: profile.id,
+                firstName: profile.name.givenName,
+                lastName: profile.name.familyName,
+                email: profile.emails[0].value,
+                image: image
             }
-        })
-    }));
 
-    passport.serializeUser((user,done) =>{
+            // Check for existing user
+            User.findOne({
+                googleID: profile.id
+            }).then(user => {
+                if(user){
+                    // Return user
+                    done(null, user);
+                } else {
+                    // Create user
+                    new User(newUser)
+                        .save()
+                        .then(user => done(null, user));
+                }
+            })
+        })
+    );
+
+    passport.serializeUser((user, done) => {
         done(null, user.id);
     });
 
-    passport.deserializeUser(function(id, done) {
-        User.findById(id).then(user => done(err, user));
+    passport.deserializeUser((id, done) => {
+        User.findById(id).then(user => done(null, user));
     });
-
-};
-
-
+}

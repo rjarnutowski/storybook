f435bc50563ac3250576eaa38e333ac9d8f51a4f
Ryan Jarnutowski <rjarno@gmail.com>
2018-03-06 09:21:00 -0500

    remove keys

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-    mongoURI:'mongodb://ryan:ryan@ds117909.mlab.com:17909/jarnobooks-dev',
-    googleClientID:'83240808045-nf7f2q03i1pv6durc0ocbhjs49033st9.apps.googleusercontent.com',
-    googleClientSecret:'i9hAKu0wSkhZ7p4ZpBTerDb0'
+if(process.env.NODE_ENV === 'production'){
+    module.exports = require('keys_prod')
+}else {
+    module.exports = require('keys_dev')
 }
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI:'mongodb://ryan:ryan@ds117909.mlab.com:17909/jarnobooks-dev',
+    googleClientID:'83240808045-nf7f2q03i1pv6durc0ocbhjs49033st9.apps.googleusercontent.com',
+    googleClientSecret:'i9hAKu0wSkhZ7p4ZpBTerDb0'
+}
